aef5ad47f16c31fb40bb72a301a8b30865ce2a3d
jonomy <ceciliabelmar12345@gmail.com>
2017-11-08 22:02:37 -0300

    CREACION CON INTERFAZ GRAFICA CON JAVA SWING

---
+++ src/javaswing/JavaSwing.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaswing;
+//paso 1 - importamos las librerias
+import javax.swing.*;//importamos todo el swing
+import java.awt.*;
+/**
+ *
+ * @author LAB06
+ * //paso 2 - heredar la clase desde JFRAME
+ */
+public class JavaSwing 
+{
+    //paso 3 - declara mi primer interfaz grafica
+   JPanel panel:// creacion de objeto de objeto panel
+        label  label; 
+     JTextF
+        boton = new JButton();
+       //paso 6 - agregar los elementos al JFrame
+        this .add (panel);
+        panel. add (label);
+        panel. add (text);
+        panel. add (boton);
+        //paso 7 - propiedades de los objetos
+        label. setText("Nombre");
+        boton. setText("Enviar");
+        panel. setBackground(Color.blue);//color de fondo del JPanel
+    }
+}
+
+    /**
+     * 
+     * 
+     * 
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        // TODO code application logic here
+    }
+    
+}
